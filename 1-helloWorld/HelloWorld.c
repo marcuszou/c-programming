c9a06b612d43393893db51ac0ef9031d81f8cb6d
Marcus Zou <marcus@MarcBookM23.local>
2024-10-26 21:53:36 +0200

    day 2

---
+++ 1-helloWorld/HelloWorld.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main1(){
+
+    /* Escape Sequence = chars conbination consisting of a backslash \
+                         followed by a letter or combination of digits.
+                         \n - newline
+                         \t - tab
+    */
+    printf("I like pizza.\n");
+    printf("It's really good\n");
+    printf("Hello World\n");
+
+    return 0;
+}
---
+++ 2-variables/HelloWorld.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    //variable
+    //
+    int x; // declaration
+    x = 10; // initialization
+    int y = 321;
+
+    int age = 21;
+    float gpa = 2.05; //floating number
+    char grade = 'C'; // single character
+    char name[] = "Bro"; //array of characters
+
+
+    printf("You are %d age years old\n", age);
+    printf("Hello %s\n", name);
+    printf("Your average grade is %c\n", grade);
+
+    return 0;
+
+}
